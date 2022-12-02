69033ce91a446b22605e9464291b204f4f8605ce
seokjin8678 <rnrn8678@naver.com>
2022-12-03 02:18:32 +0900

    fix: a 태그, 로그인 버튼 Link 태그로 변경

--- components/layout/MainNavigation.tsx
+++ components/layout/MainNavigation.tsx
@@ -9,10 +9,11 @@ const MainNavigation = (props: MainNavigationProps) => {
         <nav className="fixed w-full z-30 top-0 text-white">
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                 <div className="pl-4 flex items-center">
-                    <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-                       href="#">
+                    <Link
+                        className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+                        href="/">
                         YJ Travel Guide
-                    </a>
+                    </Link>
                 </div>
                 <div
                     className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20">
@@ -35,10 +36,9 @@ const MainNavigation = (props: MainNavigationProps) => {
                             </a>
                         </li>*/}
                     </ul>
-                    <button
-                        className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                        로그인
-                    </button>
+                    <Link className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out" href="/signin">
+                            로그인
+                    </Link>
                 </div>
             </div>
             <hr className="border-b border-gray-100 opacity-25 my-0 py-0"/>
