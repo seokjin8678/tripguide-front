b92e0602d51f1f435c895788f35b61cfce6df340
seokjin8678 <rnrn8678@naver.com>
2022-12-04 06:47:36 +0900

    feat: 회원가입 실패 시 입력창에 오류 메시지 출력

--- components/ui/MyInput.tsx
+++ components/ui/MyInput.tsx
@@ -1,14 +1,18 @@
 import React, { HTMLInputTypeAttribute } from 'react';
 import { TextInput } from 'flowbite-react';
+import { TextInputColors } from 'flowbite-react/lib/esm/components/FormControls/TextInput/TextInput';
 
 interface MyInputProps {
     id: string,
     type?: HTMLInputTypeAttribute | undefined,
     className?: string | undefined;
+    required?: boolean,
+    helperText?: string,
+    color?: keyof TextInputColors;
 }
 
 const MyInput = React.forwardRef<HTMLInputElement, MyInputProps>((props, ref) => {
-    return <TextInput className={props.className} id={props.id} type={props.type} ref={ref}/>;
+    return <TextInput color={props.color} helperText={props.helperText} required={props.required} className={props.className} id={props.id} type={props.type} ref={ref}/>;
 });
 MyInput.displayName = 'MyInput';
 export default MyInput;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,10 +1,12 @@
-import { Label } from 'flowbite-react';
+import { Button, Label, Modal } from 'flowbite-react';
 import React, { useRef, useState } from 'react';
 import Layout from '../components/layout/Layout';
 import MyInput from '../components/ui/MyInput';
-import { SignUpRequest } from '../models/SignUpRequest';
 import { api } from '../utils/axios';
 import MyButton from '../components/ui/MyButton';
+import { SignUpRequest } from '../models/SignUpRequest';
+import { useRouter } from 'next/router';
+
 
 interface SignUpProps {
 
@@ -15,16 +17,30 @@ const SignUp = (props: SignUpProps) => {
     const passwordInputRef = useRef<HTMLInputElement>(null);
     const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
     const nickNameInputRef = useRef<HTMLInputElement>(null);
+    const [emailValidation, setEmailValidation] = useState('');
+    const [nicknameValidation, setNicknameValidation] = useState('');
+    const [passwordValidation, setPasswordValidation] = useState('');
+    const [confirmPasswordValidation, setConfirmPasswordValidation] = useState('');
+    const router = useRouter();
 
     const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const url = '/api/v1/auth/signup';
-
         const enteredEmail = emailInputRef.current!.value;
         const enteredPassword = passwordInputRef.current!.value;
         const enteredConfirmPassword = confirmPasswordInputRef.current!.value;
         const enteredNickname = nickNameInputRef.current!.value;
+        setEmailValidation('');
+        setNicknameValidation('');
+        setPasswordValidation('');
+        setConfirmPasswordValidation('');
 
+        if (enteredPassword !== enteredConfirmPassword) {
+            setPasswordValidation('비밀번호와 확인 비밀빈호가 같지 않습니다!');
+            setConfirmPasswordValidation('비밀번호와 확인 비밀빈호가 같지 않습니다!');
+            return;
+        }
+
+        const url = '/api/v1/auth/signup';
         const signUpRequest: SignUpRequest = {
             email: enteredEmail,
             password: enteredPassword,
@@ -32,13 +48,63 @@ const SignUp = (props: SignUpProps) => {
             nickname: enteredNickname
         };
 
+        try {
+            const res = await api.post(url, signUpRequest);
+            if (res.status === 200) {
+                emailInputRef.current!.value = '';
+                nickNameInputRef.current!.value = '';
+                passwordInputRef.current!.value = '';
+                confirmPasswordInputRef.current!.value = '';
+                setShowModal(true);
+            }
+        } catch (err: any) {
+            const validation = err.response.data.validation;
+            Object.keys(validation).forEach(key => {
+                if (key === 'email') {
+                    setEmailValidation(validation[key])
+                }
+                if (key === 'nickname') {
+                    setNicknameValidation(validation[key])
+                }
+                if (key === 'password') {
+                    setPasswordValidation(validation[key]);
+                }
+                if (key === 'confirmPassword') {
+                    setConfirmPasswordValidation(validation[key])
+                }
+            });
+        }
+    };
 
-        let res = await api.post(url, signUpRequest);
-        console.log(res.data);
+    const [showModal, setShowModal] = useState(false);
+
+    const modalClose = () => {
+        setShowModal(false);
     };
 
     return (
         <Layout>
+            <Modal show={showModal}
+                   size="md"
+                   popup={true}
+                   onClose={modalClose}
+            >
+                <Modal.Header/>
+                <Modal.Body>
+                    <div className="text-center">
+                        <h3 className="mb-5 text-lg font-normal  dark:text-gray-400">
+                            회원가입이 정상적으로 되었습니다!
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                            <Button color="info" onClick={() => {
+                                router.push('/signin');
+                            }}>
+                                로그인 페이지로 이동
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
             <div className="body-bg min-h-screen -z-30 pt-8 md:pt-10 px-2 md:px-0">
                 <main className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
                     <section>
@@ -46,14 +112,22 @@ const SignUp = (props: SignUpProps) => {
                     </section>
                     <section className="mt-5">
                         <form className="flex flex-col" onSubmit={submitHandler}>
-                            <Label className="my-2 block" htmlFor="email" value="Email" />
-                            <MyInput id="email" type="email" ref={emailInputRef}/>
-                            <Label className="my-2 block" htmlFor="nickname" value="Nickname" />
-                            <MyInput id="nickname" ref={nickNameInputRef}/>
-                            <Label className="my-2 block" htmlFor="password" value="Password" />
-                            <MyInput id="password" type="password" ref={passwordInputRef}/>
-                            <Label className="my-2 block" htmlFor="confirmPassword" value="Confirm Password" />
-                            <MyInput id="confirmPassword" type="password"
+                            <Label className="my-2 block" htmlFor="email" value="Email"/>
+                            <MyInput required color={emailValidation ? 'failure' : undefined}
+                                     helperText={emailValidation ? emailValidation : undefined} id="email" type="email"
+                                     ref={emailInputRef}/>
+                            <Label className="my-2 block" htmlFor="nickname" value="Nickname"/>
+                            <MyInput required color={nicknameValidation ? 'failure' : undefined}
+                                     helperText={nicknameValidation ? nicknameValidation : undefined} id="nickname"
+                                     ref={nickNameInputRef}/>
+                            <Label className="my-2 block" htmlFor="password" value="Password"/>
+                            <MyInput required color={passwordValidation ? 'failure' : undefined}
+                                     helperText={passwordValidation ? passwordValidation : undefined} id="password"
+                                     type="password" ref={passwordInputRef}/>
+                            <Label className="my-2 block" htmlFor="confirmPassword" value="Confirm Password"/>
+                            <MyInput required color={confirmPasswordValidation ? 'failure' : undefined}
+                                     helperText={confirmPasswordValidation ? confirmPasswordValidation : undefined}
+                                     id="confirmPassword" type="password"
                                      ref={confirmPasswordInputRef}/>
                             <MyButton className="mt-4 bg-blue-600" type="submit">
                                 회원가입
