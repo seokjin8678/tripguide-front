4fd673fce85ffbd56bdda58adf4d4bc55c2706de
seokjin8678 <rnrn8678@naver.com>
2022-12-04 03:30:05 +0900

    feat: 회원가입 api 추가

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -3,16 +3,18 @@ import React, { HTMLInputTypeAttribute } from 'react';
 interface InputProps {
     id: string,
     type?: HTMLInputTypeAttribute | undefined,
-    children: string
+    children: string,
 }
 
-const Input = (props: InputProps) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
     return (
         <div className="mb-6 pt-3 rounded bg-gray-200">
-            <label className="block text-gray-700 text-sm font-bold mb-2 ml-3" htmlFor={props.id}>{props.children}</label>
-            <input type={props.type} id={props.id}
+            <label className="block text-gray-700 text-sm font-bold mb-2 ml-3"
+                   htmlFor={props.id}>{props.children}</label>
+            <input type={props.type} id={props.id} ref={ref}
                    className="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3"/>
         </div>
     );
-};
+});
+Input.displayName = 'Input';
 export default Input;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Layout from '../components/layout/Layout';
 import Input from '../components/ui/Input';
 import Button from '../components/ui/Button';
+import { SignUpRequest } from '../models/SignupRequest';
+import { api } from '../utils/axios';
 
 interface SignUpProps {
 
 }
 
 const SignUp = (props: SignUpProps) => {
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
+    const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
+    const nickNameInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const url = '/api/v1/auth/signup';
+
+        const enteredEmail =  emailInputRef.current!.value;
+        const enteredPassword = passwordInputRef.current!.value;
+        const enteredConfirmPassword = confirmPasswordInputRef.current!.value;
+        const enteredNickname = nickNameInputRef.current!.value;
+
+        const signUpRequest: SignUpRequest = {
+            email: enteredEmail,
+            password: enteredPassword,
+            confirmPassword: enteredConfirmPassword,
+            nickname: enteredNickname
+        }
+
+        let res = await api.post(url, signUpRequest);
+        console.log(res.data);
+    };
+
     return (
         <Layout>
             <div className="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0">
@@ -17,10 +44,12 @@ const SignUp = (props: SignUpProps) => {
                     </section>
 
                     <section className="mt-10">
-                        <form className="flex flex-col" method="POST" action="#">
-                            <Input id="email" type="email">Email</Input>
-                            <Input id="password" type="password">Password</Input>
-                            <Input id="confirmPassword" type="password">Confirm Password</Input>
+                        <form className="flex flex-col" onSubmit={submitHandler}>
+                            <Input id="email" type="email" ref={emailInputRef}>Email</Input>
+                            <Input id="nickname" ref={nickNameInputRef}>Nickname</Input>
+                            <Input id="password" type="password" ref={passwordInputRef}>Password</Input>
+                            <Input id="confirmPassword" type="password" ref={confirmPasswordInputRef}>Confirm
+                                Password</Input>
                             <Button type="submit">
                                 회원가입
                             </Button>
