74993b8d5b8a0928e57f8de5dbfab47c7c76efca
seokjin8678 <rnrn8678@naver.com>
2022-12-05 14:39:16 +0900

    feat: 여행 계획 페이지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,11 @@
 import type { NextPage } from 'next';
 import Layout from '../components/layout/Layout';
-import { useAppSelector } from '../hooks';
-import { api } from '../utils/axios';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
-    const auth = useAppSelector(state => state.auth);
-
-    const test = async () => {
-        try {
-            const res = await api.post('/api/v1/test');
-            console.log(res.data);
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
     return (
         <Layout>
-            <div className="pt-0 md:pt-24">
+            <div>
                 <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                     <div className="flex flex-col w-full justify-center items-start text-center md:text-left">
                         <p className="uppercase tracking-loose text-xl w-full">
@@ -29,10 +17,10 @@ const Home: NextPage = () => {
                         <p className="leading-normal text-2xl mb-8 w-full">
                             다른 사람은 어떤 여행 계획을 작성했을까요?
                         </p>
-                        <button onClick={test}
-                                className="mx-auto md:mx-0 bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                        <Link href="/plans"
+                              className="mx-auto md:mx-0 bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                             둘러보기
-                        </button>
+                        </Link>
                     </div>
                 </div>
             </div>
