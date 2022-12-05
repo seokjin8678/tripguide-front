a9a37fa16e18e6ed3bc1ddce037dbd30c1dec3e2
seokjin8678 <rnrn8678@naver.com>
2022-12-05 11:21:02 +0900

    fix: redux-persist 사용 시 serializable 관련 에러 수정

--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import reducer from './rootReducer'
+import reducer from './rootReducer';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 const store = configureStore({
-    reducer
-})
+    reducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    })
+});
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
