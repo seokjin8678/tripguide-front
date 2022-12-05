3a72f59770ea13556596ffe2b72d353828f9325b
seokjin8678 <rnrn8678@naver.com>
2022-12-05 10:42:07 +0900

    feat: redux-persist 적용

--- store/rootReducer.ts
+++ store/rootReducer.ts
@@ -1,9 +1,17 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import sessionStorage from 'redux-persist/lib/storage/session';
 import auth from './slices/authSlice';
+
+const persistConfig = {
+    key: 'root',
+    storage: sessionStorage,
+    whitelist: ['auth']
+};
 
 const reducer = combineReducers({
     auth
 });
 
 export type ReducerType = ReturnType<typeof reducer>;
-export default reducer;
+export default persistReducer(persistConfig, reducer);
